47eaa689624fcc475611f526da17025246340ec1
qingshan <lichao@bigbaser.com>
2017-09-25 18:43:07 +0800

    sogou-spider

--- curl.php
+++ curl.php
@@ -8,7 +8,7 @@
 
 class Curl{
 
-    function curl_get($url)
+    static function curl_get($url,$header_data = [],$time_out = 0)
     {
         //初始化
         $ch = curl_init();
@@ -16,6 +16,8 @@ class Curl{
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch,CURLOPT_HTTPHEADER,$header_data);
+        curl_setopt($ch,CURLOPT_TIMEOUT,$time_out);
         //执行并获取HTML文档内容
         $output = curl_exec($ch);
         //释放curl句柄
@@ -24,7 +26,7 @@ class Curl{
         return $output;
     }
 
-    function curl_post($url,$post_data)
+    static function curl_post($url,$post_data,$header_data = [],$time_out = 0)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -33,9 +35,51 @@ class Curl{
         curl_setopt($ch, CURLOPT_POST, 1);
         // post的变量
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+        curl_setopt($ch,CURLOPT_HTTPHEADER,$header_data);
+        curl_setopt($ch,CURLOPT_TIMEOUT,$time_out);
         $output = curl_exec($ch);
         curl_close($ch);
         //打印获得的数据
         return $output;
+    }
+
+    ////参数1：访问的URL，参数2：post数据(不填则为GET)，参数3：提交的$cookies,参数4：是否返回$cookies
+    static function curl_request($url,$post=[],$header_data = [],$cookie=[], $returnCookie=0){
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        //curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36)');
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
+        curl_setopt($curl, CURLOPT_REFERER, "http://XXX");
+        curl_setopt($curl,CURLOPT_HTTPHEADER,$header_data);
+        if($post) {
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));
+        }
+        if($cookie) {
+            curl_setopt($curl, CURLOPT_COOKIE, $cookie);
+        }
+        curl_setopt ($curl, CURLOPT_PROXY, self::get_proxy());
+        curl_setopt($curl, CURLOPT_HEADER, $returnCookie);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $data = curl_exec($curl);
+        if (curl_errno($curl)) {
+             return curl_error($curl);
+        }
+        curl_close($curl);
+        if($returnCookie){
+            list($header, $body) = explode("\r\n\r\n", $data, 2);
+            preg_match_all("/Set\-Cookie:([^;]*);/", $header, $matches);
+            $info['cookie']  = substr($matches[1][0], 1);
+            $info['content'] = $body;
+            return $info;
+        }else{
+            return $data;
+        }
+    }
+    static function get_proxy(){
+
+        return '';
     }
 }
